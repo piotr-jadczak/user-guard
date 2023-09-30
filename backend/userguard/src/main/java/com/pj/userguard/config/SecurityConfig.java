f6ec6096cb72114c5a211cf4f7a5b95fbc62745c
piotr-jadczak <58400623+piotr-jadczak@users.noreply.github.com>
2023-10-01 00:55:13 +0200

    add authorization

--- backend/userguard/src/main/java/com/pj/userguard/config/SecurityConfig.java
+++ backend/userguard/src/main/java/com/pj/userguard/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.pj.userguard.config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+@AllArgsConstructor
 @EnableMethodSecurity
 @Configuration
 public class SecurityConfig {
@@ -19,11 +24,22 @@ public class SecurityConfig {
     }
 
     @Bean
+    @Order(1)
     public SecurityFilterChain appSecurityFilterChain(HttpSecurity http) throws Exception {
+        // endpoints security
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
 
-        http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+        // cors
         http.cors(AbstractHttpConfigurer::disable);
+
+        // csrf
         http.csrf(AbstractHttpConfigurer::disable);
+
+        // session management
+        http.sessionManagement(c -> c.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        // authentication type
+        http.httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
